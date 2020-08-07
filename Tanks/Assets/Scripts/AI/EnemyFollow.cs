2e29ffbb1c02a5889a38b48b9eea4b7753dfb46a
dbzheng <dbzheng@umich.edu>
2020-08-06 21:55:58 -0400

    finished basic movements

---
+++ Tanks/Assets/Scripts/AI/EnemyFollow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public float speed = 10;
+    public Transform player; 
+    private void Update() { 
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
+}
